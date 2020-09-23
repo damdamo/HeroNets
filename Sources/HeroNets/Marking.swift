af85ee95fecaa95ec73bca81ebe1b3865f4499fc
Damien Morard <damien.morard@unige.ch>
2020-09-23 17:48:35 +0200

    Remove useless file

--- Sources/HeroNets/Marking.swift
+++
@@ -1,8 +0,0 @@
-//public struct Marking: Comparable {
-//  public static func < (lhs: Marking, rhs: Marking) -> Bool {
-//    <#code#>
-//  }
-//  
-//  let places: [Place: [String: Int]]
-//  
-//}
