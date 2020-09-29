f8c3f777be0c4ab54470a179dd057d4f7e5f0e61
Damien Morard <damien.morard@unige.ch>
2020-09-29 11:02:32 +0200

    Add dependencies in Package swift

--- Package.swift
+++ Package.swift
@@ -13,14 +13,20 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kyouko-taiga/AlpineLang.git", .branch("master")),
+        .package(url: "https://github.com/kyouko-taiga/DDKit.git", .branch("master")),
+        //.product(name: "AlpineLib", package: "AlpineLang"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "HeroNets",
-            dependencies: []),
+            dependencies:
+            [
+              .product(name: "AlpineLib", package: "AlpineLang"),
+              "DDKit"
+            ]),
         .testTarget(
             name: "HeroNetsTests",
             dependencies: ["HeroNets"]),
